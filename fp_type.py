5df001d98484c6d773fc6f5571a6c4ad2a1f621f
Aaron Parsons <aparsons@berkeley.edu>
2020-07-16 14:05:02 -0700

    Added comments to FixedPointType

--- fp_type.py
+++ fp_type.py
@@ -1,28 +1,42 @@
 import numpy as np
 
 class FixedPointType:
+    '''Data type for fixed-point binary arithmetic.'''
     def __init__(self, bit_width, bin_point):
         self.bit_width = bit_width
         self.bin_point = bin_point
     def promote(self, fpt):
+        '''Return a new FixedPointType that can losslessly represent both
+        the current and the provided FixedPointTypes.'''
         whole_bits = max([fpt.bit_width - fpt.bin_point,
                           self.bit_width - self.bin_point])
         bin_point = max([self.bin_point, fpt.bin_point])
         bit_width = bin_point + whole_bits
         return FixedPointType(bit_width, bin_point)
     def __add__(self, fpt):
+        '''Return the data type resulting from summing this data type
+        with the provided one, including the carry bit.'''
         fpt = self.promote(fpt)
         fpt.bit_width += 1
         return fpt
     def __mul__(self, fpt):
+        '''Return the data type resulting from multiplying this data type
+        with the provided one.'''
         bit_width = self.bit_width + fpt.bit_width
         bin_point = self.bin_point + fpt.bin_point
         return FixedPointType(bit_width, bin_point)
     def to_float(self, data):
+        '''Convert the provided integer or numpy integer array into
+        the its floating-point value according to the bit width and
+        binary point of this data type.'''
         data = (data % 2**self.bit_width).astype(np.float)
         data /= 2**self.bin_point
         return data
     def cast(self, data, fpt_in=None):
+        '''Cast the provided integer or numpy integer array into
+        the fixed-point representation corresponding to this data type.
+        Optionally provide the fixed-point data type of the orignal array
+        to get the binary point right.'''
         if fpt_in is not None:
             if fpt_in.bin_point > self.bin_point:
                 data = data >> (fpt_in.bin_point - self.bin_point)
@@ -30,14 +44,3 @@ class FixedPointType:
                 data = data << (self.bin_point - fpt_in.bin_point)
         data = data % 2**self.bit_width
         return data
-
-#def butterfly_r2(d_in, stage, dtype_in, dtype_out, dtype_tw):
-#    d_in.shape = (2**stage, -1)
-#    d1 = d_in[0::2].flatten()
-#    d2 = d_in[1::2].flatten()
-#    tw = twiddle(stage, stages, dtype_tw)
-#    d2 *= tw
-#    top = d1 + d2
-#    bot = d1 - d2
-#    d_out = np.concatenate([top, bot], axis=-1)
-#    d_out = fp_cast(dout, dtype_out)
